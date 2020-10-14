42ed36021211e75321260379e714a2dca15ad243
timlunar <lunar.tim@gmail.com>
2020-10-14 13:47:14 +0200

    "TODO comment"

--- app/src/main/java/com/example/mybudget/LoginActivity.java
+++ app/src/main/java/com/example/mybudget/LoginActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+    /*TODO*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
