dc04df4d09f15f35a120086ca431f2f86993f8b4
Zan <zan19.jeric@gmail.com>
2020-10-19 21:08:26 +0200

    Add Page Up Animation in bottom nav

--- app/src/main/java/com/example/mybudget/LoginActivity.java
+++ app/src/main/java/com/example/mybudget/LoginActivity.java
@@ -2,13 +2,24 @@ package com.example.mybudget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-    /*TODO*/
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginBtn = (Button)findViewById(R.id.buttonLogin);
+        loginBtn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/mybudget/MainActivity.java
+++ app/src/main/java/com/example/mybudget/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.mybudget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton btnOpenPage = findViewById(R.id.btnOpenPage);
+        btnOpenPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this, R.style.Theme_Design_BottomSheetDialog);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_page_up,(LinearLayout)findViewById(R.id.pageUpContainer));
+                bottomSheetView.findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
     }
 }
