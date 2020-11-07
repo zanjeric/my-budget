19fda653b66e551275e0381fdc0e3ef71a0fd697
Jure Casar <jure.casar1@gmail.com>
2020-11-07 11:36:00 +0100

    added signout button

--- app/src/main/java/com/example/mybudget/AccountActivity.java
+++ app/src/main/java/com/example/mybudget/AccountActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -58,7 +59,7 @@ public class AccountActivity extends AppCompatActivity {
             }
         });
 
-        Button googleSignInBtn = (Button)findViewById(R.id.btnGoogleSignIn);
+        SignInButton googleSignInBtn = findViewById(R.id.btnGoogleSignIn);
         googleSignInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
