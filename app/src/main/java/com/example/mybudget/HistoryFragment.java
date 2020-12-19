ed84bbe851a17b5d2c9e9624ab2e35bc70f280ae
Jure Casar <jure.casar1@gmail.com>
2020-12-19 15:28:31 +0100

    added viewpager

--- app/src/main/java/com/example/mybudget/HistoryFragment.java
+++ app/src/main/java/com/example/mybudget/HistoryFragment.java
@@ -8,13 +8,44 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class HistoryFragment extends Fragment {
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
 
+        viewPager = view.findViewById(R.id.viewPager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager(), this.getLifecycle());
+        viewPager.setAdapter(pagerAdapter);
         return view;
     }
+
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm, Lifecycle lc) {
+            super(fm, lc);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            return new HistorySlideFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/mybudget/HistorySlideFragment.java
@@ -0,0 +1,21 @@
+package com.example.mybudget;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HistorySlideFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(
+                R.layout.fragment_history_slide, container, false);
+    }
+
+
+}
