6a2022f03d7ee7ac6864022708a74784406329b9
Jure Casar <jure.casar1@gmail.com>
2020-12-21 14:27:32 +0100

    per month slides are created and all necessary info is passed to them

--- app/src/main/java/com/example/mybudget/HistoryFragment.java
+++ app/src/main/java/com/example/mybudget/HistoryFragment.java
@@ -1,6 +1,8 @@
 package com.example.mybudget;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,24 +24,29 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HistoryFragment extends Fragment {
 
-    private static final int NUM_PAGES = 3;
     private static final String TAG = "History/transactions";
     private ViewPager2 viewPager;
     private FragmentStateAdapter pagerAdapter;
 
     private FirebaseFirestore db;
-    private List<Transaction> transactions;
+    private List<TransactionParcel> transactions;
 
     @Nullable
     @Override
@@ -47,8 +54,6 @@ public class HistoryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
 
         viewPager = view.findViewById(R.id.viewPager);
-        pagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager(), this.getLifecycle());
-        viewPager.setAdapter(pagerAdapter);
 
         /* Firestore Database */
         db = FirebaseFirestore.getInstance();
@@ -70,19 +75,41 @@ public class HistoryFragment extends Fragment {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             categories.put(document.getReference().getId(), (String) document.get("name"));
                         }
-                        transactionRef.whereEqualTo("UID", userRef).get().addOnCompleteListener(t -> {
-                            for (QueryDocumentSnapshot document : t.getResult()) {
-                                String categoryName = categories.get(((DocumentReference) document.get("category")).getId());
+                        transactionRef.whereEqualTo("UID", userRef).orderBy("date", Query.Direction.DESCENDING).get().addOnCompleteListener(t -> {
+                            if (t.isSuccessful()) {
+                                pagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager(), this.getLifecycle());
+                                int mCount = 0;
+                                String prevMonth = "xx";
 
-                                Log.d("Transaction.category", "" + categoryName);
-                                Timestamp timestamp = (Timestamp) document.get("date");
-                                String date = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(timestamp.toDate());
+                                List<TransactionParcel> perMonth = new ArrayList<>();
+                                for (QueryDocumentSnapshot document : t.getResult()) {
+                                    String categoryName = categories.get(((DocumentReference) document.get("category")).getId());
+                                    Log.d("Transaction.category", "" + categoryName);
 
-                                Log.d("Transaction.date", date);
-                                int amount = Integer.parseInt(document.get("amount").toString());
+                                    Timestamp timestamp = (Timestamp) document.get("date");
+                                    String date = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(timestamp.toDate());
+                                    Log.d("Transaction.date", date);
 
-                                Log.d("Transaction.amount", String.valueOf(amount));
-                                transactions.add(new Transaction(uid,""+categoryName,date,"",amount));
+                                    int amount = Integer.parseInt(document.get("amount").toString());
+                                    Log.d("Transaction.amount", String.valueOf(amount));
+
+                                    TransactionParcel T = new TransactionParcel("" + categoryName, date, amount);
+
+                                    if(!date.startsWith(prevMonth)) {
+                                        if(!prevMonth.equals("xx"))
+                                            ((ScreenSlidePagerAdapter) pagerAdapter).addFragment(perMonth.toArray(new Parcelable[0]));
+                                        prevMonth = date.substring(0, 2);
+
+                                        perMonth = new ArrayList<>();
+                                        mCount++;
+                                    }
+                                    perMonth.add(T);
+                                }
+                                ((ScreenSlidePagerAdapter) pagerAdapter).addFragment(perMonth.toArray(new Parcelable[0]));
+
+                                Log.d("Transaction.mCount", String.valueOf(mCount));
+                                pagerAdapter.saveState();
+                                viewPager.setAdapter(pagerAdapter);
                             }
                         });
                     }
@@ -92,19 +119,37 @@ public class HistoryFragment extends Fragment {
     }
 
 
-    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+    private static class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+
+        private ArrayList<Fragment> items = new ArrayList<>();
+
         public ScreenSlidePagerAdapter(FragmentManager fm, Lifecycle lc) {
             super(fm, lc);
         }
 
+        public void addFragment(Parcelable[] transactions) {
+            if(transactions.length == 0) return;
+
+            // Get full name of the month
+            int monthIndex = Integer.parseInt(((TransactionParcel) transactions[0]).getDate().substring(0, 2));
+            String month = new DateFormatSymbols().getMonths()[monthIndex-1];
+
+            Fragment f = new HistorySlideFragment();
+            Bundle bundle = new Bundle();
+            bundle.putParcelableArray("transactions", transactions);
+            bundle.putString("month", month);
+            f.setArguments(bundle);
+            items.add(f);
+        }
+
         @Override
         public Fragment createFragment(int position) {
-            return new HistorySlideFragment();
+            return items.get(position);
         }
 
         @Override
         public int getItemCount() {
-            return NUM_PAGES;
+            return items.size();
         }
     }
 
--- app/src/main/java/com/example/mybudget/HistorySlideFragment.java
+++ app/src/main/java/com/example/mybudget/HistorySlideFragment.java
@@ -2,17 +2,42 @@ package com.example.mybudget;
 
 
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HistorySlideFragment extends Fragment {
 
+    private Parcelable[] transactions;
+    private String month;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        transactions = getArguments().getParcelableArray("transactions");
+        month = getArguments().getString("month");
+        Log.d("HistorySlide.month", month);
+//        for (Parcelable t :
+//                transactions) {
+//            Log.d("Transaction.Parcel.info", ((TransactionParcel) t).getDate());
+//        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return (ViewGroup) inflater.inflate(R.layout.fragment_history_slide, container, false);
+        View view = inflater.inflate(R.layout.fragment_history_slide, container, false);
+
+        TextView monthText = view.findViewById(R.id.monthTitle);
+        monthText.setText(month);
+
+        return view;
     }
 
 
